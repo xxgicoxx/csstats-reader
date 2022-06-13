b1ed9da4fd491144c0ce50a3bfb9e28e168951de
Giovani de Oliveira <giovanioliveira@outlook.com.br>
2022-06-13 10:00:47 -0300

    Remove example

--- example/usage.js
+++
@@ -1,9 +0,0 @@
-const CSStats = require('../index');
-const path = require('path');
-
-const csstats = new CSStats();
-
-(async () => {
-    const data = await csstats.parse(path.resolve(__dirname, './data/csstats.dat'));
-    console.log(data);
-})();
